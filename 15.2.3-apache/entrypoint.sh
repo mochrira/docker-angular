35805ed54d3860c9e881599757bb9d3eff6905ab
mochrira <mochrira@gmail.com>
2023-04-03 01:22:38 +0700

    make angular ready for production

--- 15.2.3-apache/entrypoint.sh
+++ 15.2.3-apache/entrypoint.sh
@@ -4,54 +4,15 @@ REINSTALL_MODULES=${REINSTALL_MODULES:-NO}
 NG_CONFIGURATION=${NG_CONFIGURATION:-development}
 NG_REBUILD=${NG_REBUILD:-NO}
 
-DEFAULT_FILE_CONTENT="<!DOCTYPE html>
-<html lang=\"en\">
-<head>
-    <meta charset=\"UTF-8\">
-    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
-    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-    <title>Angular</title>
-</head>
-<body>
-    <h1>Empty Project</h1>
-    <p>Your project folder still empty. Use <a href=\"https://angular.io/cli\">Angular CLI</a> to generate new angular project</p>
-</body>
-</html>"
+if [ $PRODUCTION == "YES" ]; then
+    sudo -E /build.sh > /dev/stdout 2>&1 &
 
-if [ ! -f "angular.json" ]; then
-    if [ ! -d "dist" ]; then
-        if [ $PRODUCTION == "YES" ]; then sudo mkdir -p dist; else mkdir -p dist; fi
-    fi
-
-    if [ ! -f "dist/index.html" ]; then
-        if [ $PRODUCTION == "YES" ]; then sudo -E echo "${DEFAULT_FILE_CONTENT}" > dist/index.html; else echo "${DEFAULT_FILE_CONTENT}" > dist/index.html; fi
-    fi
+    if [ ! -d "/var/www/html" ]; then sudo mkdir -p /var/www/html; fi
+    sudo sed -i 's#/home/node/app/dist#/var/www/html#g' /etc/apache2/httpd.conf
 fi
 
-if [ -d "./.docker" ]; then
-    if [ -f "./.docker/before_build.js" ]; then
-        if [ $PRODUCTION == "YES" ]; then sudo -E node ./.docker/before_build.js; else node ./.docker/before_build.js; fi
-    fi
-fi
-
-if [[ -f "angular.json" && -f "package.json" ]]; then
-    if [[ ! -d "node_modules" || $REINSTALL_MODULES == "YES" ]]; then
-        if [ $PRODUCTION == "YES" ]; then sudo npm install; else npm install; fi
-    fi
-
-    if [[ ! -d "dist" || $NG_REBUILD == "YES" ]]; then
-        if [ $PRODUCTION == "YES" ]; then
-            sudo ng build --configuration $NG_CONFIGURATION --output-path=dist/
-        else
-            ng build --configuration $NG_CONFIGURATION --output-path=dist/
-        fi
-    fi
-fi
-
-if [ -d "./.docker" ]; then
-    if [ -f "./.docker/after_build.js" ]; then
-        if [ $PRODUCTION == "YES" ]; then sudo -E node ./.docker/after_build.js; else node ./.docker/after_build.js; fi
-    fi
+if [ $PRODUCTION == "NO" ]; then
+    /build.sh
 fi
 
 sudo /usr/sbin/httpd -DFOREGROUND
--- 15.2.3-nginx/entrypoint.sh
+++ 15.2.3-nginx/entrypoint.sh
@@ -4,54 +4,38 @@ REINSTALL_MODULES=${REINSTALL_MODULES:-NO}
 NG_CONFIGURATION=${NG_CONFIGURATION:-development}
 NG_REBUILD=${NG_REBUILD:-NO}
 
-DEFAULT_FILE_CONTENT="<!DOCTYPE html>
-<html lang=\"en\">
-<head>
-    <meta charset=\"UTF-8\">
-    <meta http-equiv=\"X-UA-Compatible\" content=\"IE=edge\">
-    <meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">
-    <title>Angular</title>
-</head>
-<body>
-    <h1>Empty Project</h1>
-    <p>Your project folder still empty. Use <a href=\"https://angular.io/cli\">Angular CLI</a> to generate new angular project</p>
-</body>
-</html>"
+if [ $PRODUCTION == "YES" ]; then
+    sudo -E /build.sh > /dev/stdout 2>&1 &
 
-if [ ! -f "angular.json" ]; then
-    if [ ! -d "dist" ]; then
-        if [ $PRODUCTION == "YES" ]; then sudo mkdir -p dist; else mkdir -p dist; fi
-    fi
-
-    if [ ! -f "dist/index.html" ]; then
-        if [ $PRODUCTION == "YES" ]; then sudo -E echo "${DEFAULT_FILE_CONTENT}" > dist/index.html; else echo "${DEFAULT_FILE_CONTENT}" > dist/index.html; fi
-    fi
+    if [ ! -d "/var/www/html" ]; then sudo mkdir -p /var/www/html; fi
+    
+    sudo echo \
+    $'server { \n
+        listen 80 default_server; \n
+        listen [::]:80 default_server; \n
+        try_files $uri $uri/ /index.html; \n
+    \n
+        root /var/www/html; \n
+        index index.html; \n
+    \n
+        include /home/node/app/.docker/nginx/*[.]conf; \n
+    }' | sudo tee /etc/nginx/http.d/default.conf > /dev/null
 fi
 
-if [ -d "./.docker" ]; then
-    if [ -f "./.docker/before_build.js" ]; then
-        if [ $PRODUCTION == "YES" ]; then sudo -E node ./.docker/before_build.js; else node ./.docker/before_build.js; fi
-    fi
-fi
+if [ $PRODUCTION == "NO" ]; then
+    /build.sh
 
-if [[ -f "angular.json" && -f "package.json" ]]; then
-    if [[ ! -d "node_modules" || $REINSTALL_MODULES == "YES" ]]; then
-        if [ $PRODUCTION == "YES" ]; then sudo npm install; else npm install; fi
-    fi
-
-    if [[ ! -d "dist" || $NG_REBUILD == "YES" ]]; then
-        if [ $PRODUCTION == "YES" ]; then
-            sudo ng build --configuration $NG_CONFIGURATION --output-path=dist/
-        else
-            ng build --configuration $NG_CONFIGURATION --output-path=dist/
-        fi
-    fi
-fi
-
-if [ -d "./.docker" ]; then
-    if [ -f "./.docker/after_build.js" ]; then
-        if [ $PRODUCTION == "YES" ]; then sudo -E node ./.docker/after_build.js; else node ./.docker/after_build.js; fi
-    fi
+    sudo echo \
+    $'server { \n
+        listen 80 default_server; \n
+        listen [::]:80 default_server; \n
+        try_files $uri $uri/ /index.html; \n
+    \n
+        root /home/node/app/dist; \n
+        index index.html; \n
+    \n
+        include /home/node/app/.docker/nginx/*[.]conf; \n
+    }' | sudo tee /etc/nginx/http.d/default.conf > /dev/null
 fi
 
 sudo /usr/sbin/nginx -g 'daemon off;'
